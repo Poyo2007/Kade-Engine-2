be723db631d903064fc9e06e3cc6b0bf004f5d84
Poyo2007 <itssng7@gmail.com>
2022-12-06 03:14:53 -0500

    either a huge error wa ll or not

--- source/Highscore.hx
+++ source/Highscore.hx
@@ -26,12 +26,6 @@ class Highscore
 
 	public static function saveWeekScore(week:Int = 1, score:Int = 0, ?diff:Int = 0):Void
 	{
-
-		#if !switch
-		NGio.postScore(score, "Week " + week);
-		#end
-
-
 		var daWeek:String = formatSong('week' + week, diff);
 
 		if (songScores.exists(daWeek))
