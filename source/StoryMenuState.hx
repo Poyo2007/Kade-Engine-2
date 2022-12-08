13dc911ea79b7976a90e4c9c1b07033206baf870
Poyo2007 <itssng7@gmail.com>
2022-12-07 19:02:30 -0500

    some optimization

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -74,6 +74,9 @@ class StoryMenuState extends MusicBeatState
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		#if desktop
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("In the Menus", null);
